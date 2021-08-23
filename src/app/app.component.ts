2e8b723400848f24149626b0da4a81c8f6b8363a
simran123-alt <simranbatra006123@gmail.com>
2021-08-23 20:17:42 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'Angular';
+  constructor() {
+    setTimeout(() => {
+      this.title = 'Title Changed';
+    }, 1000);
+  }
 }
